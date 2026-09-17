2ab610d27da92d4c0d7f05c3a3c11cf6fc32c1d8
luisgrasso <grasso.luis@gmail.com>
2026-09-17 20:13:08 -0300

    feat(home): route Mendoza informational home through shared composition

--- static/src/main.tsx
+++ static/src/main.tsx
@@ -1,11 +1,40 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import * as React from "react";
+import { StrictMode, createElement, createRoot } from "react";
 
 import App from "./App.tsx";
+import MendozaHomeShell from "./theme/MendozaHomeShell.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+function RootRouter() {
+  const [path, setPath] = React.useState(() => window.location.pathname);
+
+  React.useEffect(() => {
+    const onPopState = () => setPath(window.location.pathname);
+    const originalPushState = window.history.pushState.bind(window.history);
+    const originalReplaceState = window.history.replaceState.bind(window.history);
+
+    window.history.pushState = (...args) => {
+      originalPushState(...args);
+      window.dispatchEvent(new PopStateEvent("popstate"));
+    };
+    window.history.replaceState = (...args) => {
+      originalReplaceState(...args);
+      window.dispatchEvent(new PopStateEvent("popstate"));
+    };
+
+    window.addEventListener("popstate", onPopState);
+    return () => {
+      window.history.pushState = originalPushState;
+      window.history.replaceState = originalReplaceState;
+      window.removeEventListener("popstate", onPopState);
+    };
+  }, []);
+
+  return path === "/" ? createElement(MendozaHomeShell) : createElement(App);
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RootRouter />
   </StrictMode>,
 );
