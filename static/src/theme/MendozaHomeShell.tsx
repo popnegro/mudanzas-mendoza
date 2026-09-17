678b4259d5729a9e3d2fb5a84a6c23ba4f921ed9
luisgrasso <grasso.luis@gmail.com>
2026-09-17 20:13:02 -0300

    refactor(theme): keep Mendoza home shell presentation-only

--- static/src/theme/MendozaHomeShell.tsx
+++ static/src/theme/MendozaHomeShell.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import SEO from '../components/SEO';
 import Header from '../components/layout/Header';
 import Footer from '../components/Footer';
@@ -7,30 +6,6 @@ import { destinations } from '../data/destinations';
 import { MendozaHome } from './MendozaHome';
 
 export default function MendozaHomeShell() {
-  const [path, setPath] = React.useState(() => window.location.pathname);
-
-  React.useEffect(() => {
-    const onPopState = () => setPath(window.location.pathname);
-    const originalPushState = window.history.pushState.bind(window.history);
-    const originalReplaceState = window.history.replaceState.bind(window.history);
-    window.history.pushState = (...args) => {
-      originalPushState(...args);
-      window.dispatchEvent(new PopStateEvent('popstate'));
-    };
-    window.history.replaceState = (...args) => {
-      originalReplaceState(...args);
-      window.dispatchEvent(new PopStateEvent('popstate'));
-    };
-    window.addEventListener('popstate', onPopState);
-    return () => {
-      window.history.pushState = originalPushState;
-      window.history.replaceState = originalReplaceState;
-      window.removeEventListener('popstate', onPopState);
-    };
-  }, []);
-
-  if (path !== '/') return null;
-
   const onNavigate = (slug: string) => {
     let next = '/';
     if (slug === 'blog') next = '/blog';
